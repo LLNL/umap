31037df7cfac48211d82dd40b75cb3420e02ca59
Marty McFadden <mcfadden8@llnl.gov>
2018-06-06 11:46:02 -0700

    1. umapsort now chooses sort direction based on first word of data 2. improved umapsort validation mechanism since data is known 3. Fixed bug in umap() where residual pages were not being    accounted for.

--- tests/umapsort/umapsort.cpp
+++ tests/umapsort/umapsort.cpp
@@ -35,6 +35,8 @@
 #include "testoptions.h"
 #include "PerFile.h"
 
+bool sort_ascending = true;
+
 static inline uint64_t getns(void)
 {
   struct timespec ts;
@@ -43,24 +45,24 @@ static inline uint64_t getns(void)
   return (((uint64_t)ts.tv_sec) * 1000000000ULL) + ts.tv_nsec;
 }
 
-void initdata(uint64_t *region, int64_t rlen) {
-  fprintf(stdout, "initdata: %p, %ld\n", region, rlen);
+void initdata(uint64_t *region, uint64_t rlen) {
+  fprintf(stdout, "initdata: %p, %llu\n", region, rlen);
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<uint64_t> rnd_int;
 #pragma omp parallel for
-  for(int64_t i=0; i< rlen; ++i) {
-    region[i] = (uint64_t) (rlen - i);// rnd_int(gen);
-    //region[i] = rnd_int(gen);
+  for(uint64_t i=0; i < rlen; ++i) {
+    region[i] = (uint64_t) (rlen - i);
   }
 }
 
 void validatedata(uint64_t *region, uint64_t rlen) {
+  if (sort_ascending == true) {
 #pragma omp parallel for
-    for(uint64_t i=1; i< rlen; ++i) {
-        if(region[i] < region[i-1]) {
-            fprintf(stderr, "Worker %d found an error at index %lu, %lu is lt %lu!\n", 
-                            omp_get_thread_num(), i, region[i], region[i-1]);
+    for(uint64_t i = 0; i < rlen; ++i) {
+        if (region[i] != (i+1)) {
+            fprintf(stderr, "Worker %d found an error at index %lu, %lu != lt %lu!\n", 
+                            omp_get_thread_num(), i, region[i], i+1);
 
             if (i < 3) {
                 fprintf(stderr, "Context ");
@@ -83,17 +85,47 @@ void validatedata(uint64_t *region, uint64_t rlen) {
             }
         }
     }
+  }
+  else {
+#pragma omp parallel for
+    for(uint64_t i = 0; i < rlen; ++i) {
+        if(region[i] != (rlen - i)) {
+            fprintf(stderr, "Worker %d found an error at index %lu, %lu != %lu!\n", 
+                            omp_get_thread_num(), i, region[i], (rlen - i));
+
+            if (i < 3) {
+                fprintf(stderr, "Context ");
+                for (int j=0; j < 7; j++) {
+                    fprintf(stderr, "%lu ", region[j]);
+                }
+                fprintf(stderr, "\n");
+            }
+            else if (i > (rlen-4)) {
+                fprintf(stderr, "Context ");
+                for (uint64_t j=rlen-8; j < rlen; j++) {
+                    fprintf(stderr, "%lu ", region[j]);
+                }
+                fprintf(stderr, "\n");
+            }
+            else {
+                fprintf(stderr, 
+                    "Context i-3 i-2 i-1 i i+1 i+2 i+3:%lu %lu %lu %lu %lu %lu %lu\n",
+                    region[i-3], region[i-2], region[i-1], region[i], region[i+1], region[i+2], region[i+3]);
+            }
+        }
+    }
+  }
 }
 
 int main(int argc, char **argv)
 {
   umt_optstruct_t options;
-  long pagesize;
-  int64_t totalbytes;
+  uint64_t pagesize;
+  uint64_t totalbytes;
   uint64_t arraysize;
   void* base_addr;
 
-  pagesize = umt_getpagesize();
+  pagesize = (uint64_t)umt_getpagesize();
 
   umt_getoptions(&options, argc, argv);
 
@@ -103,10 +135,10 @@ int main(int argc, char **argv)
   base_addr = PerFile_openandmap(&options, totalbytes);
   assert(base_addr != NULL);
  
-  fprintf(stdout, "%lu pages, %lu threads\n", options.numpages, options.numthreads);
+  fprintf(stdout, "%lu pages, %llu bytes, %lu threads\n", options.numpages, totalbytes, options.numthreads);
 
   uint64_t *arr = (uint64_t *) base_addr; 
-  arraysize = totalbytes/sizeof(int64_t);
+  arraysize = totalbytes/sizeof(uint64_t);
 
   uint64_t start = getns();
   if ( !options.noinit ) {
@@ -118,7 +150,18 @@ int main(int argc, char **argv)
   if ( !options.initonly ) 
   {
     start = getns();
-    __gnu_parallel::sort(arr, &arr[arraysize],__gnu_parallel::quicksort_tag());
+    printf("%lu\n", arr[0]);
+    sort_ascending = (arr[0] != 1);
+    
+    if (sort_ascending == true) {
+      printf("Sorting in Ascending Order\n");
+      __gnu_parallel::sort(arr, &arr[arraysize], std::less<uint64_t>(), __gnu_parallel::quicksort_tag());
+    }
+    else {
+      printf("Sorting in Descending Order\n");
+      __gnu_parallel::sort(arr, &arr[arraysize], std::greater<uint64_t>(), __gnu_parallel::quicksort_tag());
+    }
+
     fprintf(stdout, "Sort took %f us\n", (double)(getns() - start)/1000000.0);
 
     start = getns();
