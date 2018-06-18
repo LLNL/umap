4eaf5ef6e58b082eb239dab0cd82c0aebdd4e9db
Marty McFadden <mcfadden8@llnl.gov>
2018-06-18 10:00:41 -0700

    Added regression tests script

---
+++ tests/regression_tests/run_regression_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# This script will run the umapsort and churn tests to
+# verify that read/write/paging is still working as
+# expected.  These tests currently only test
+# a single file.  As multi-file tests increase (FITS)
+# additional tests will be added.
+#
+churn --directio -f /mnt/intel/regression_test_churn.dat -b 10000 -c 20000 -l 1000 -d 60 
+umapsort -p 100000 -b 90000 -f /mnt/intel/regression_test_sort.dat --directio -t 128
+
+/bin/rm -f /mnt/intel/regression_test_churn.dat /mnt/intel/regression_test_sort.dat
