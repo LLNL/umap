8003e312e41618622b5866b0e15f310a1ec991a4
Keita Iwabuchi <iwabuchi1@llnl.gov>
2018-06-28 15:38:20 -0700

    Add a test case for median calculation in run_regression_tests.sh

--- tests/regression_tests/run_regression_tests.sh
+++ tests/regression_tests/run_regression_tests.sh
@@ -10,3 +10,9 @@ churn --directio -f /tmp/regression_test_churn.dat -b 10000 -c 20000 -l 1000 -d 
 umapsort -p 100000 -b 90000 -f /tmp/regression_test_sort.dat --directio -t 128
 
 /bin/rm -f /tmp/regression_test_churn.dat /tmp/regression_test_sort.dat
+
+# Test for median calculation using fits files
+tar -xvf ../median_calculation/data/test_fits_files.tar.gz -C /tmp/
+test_median_calculation -f /tmp/test_fits_files/asteroid_sim_epoch
+/bin/rm -f /tmp/test_fits_files/*
+/bin/rmdir  /tmp/test_fits_files
