e6ed47b35927e0b2d5a6a1534de81ddf63261513
Marty McFadden <mcfadden8@llnl.gov>
2018-07-17 09:24:28 -0700

    Another update

--- host-configs/toss_3_x86_64/gcc_4_8_5.cmake
+++ host-configs/toss_3_x86_64/gcc_4_8_5.cmake
@@ -1,4 +1,3 @@
-#!/bin/bash
 ##############################################################################
 # This file is part of UMAP.  For copyright information see the COPYRIGHT file in the top level directory, or at
 # https://github.com/LLNL/umap/blob/master/COPYRIGHT This program is free software; you can redistribute it and/or 
@@ -9,29 +8,6 @@
 # GNU Lesser General Public License along with this program; if not, write to the Free Software Foundation, Inc., 
 # 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
+set(CMAKE_CXX_COMPILER "g++" CACHE PATH "")
+set(CMAKE_C_COMPILER "gcc" CACHE PATH "")
 
-cd `dirname $0`
-
-export UMAP_DIR=$(git rev-parse --show-toplevel)
-export BUILD_DIR=build-${SYS_TYPE}
-
-export COMPILER=${1:-gcc_4_8_5}
-export BUILD_TYPE=${2:-Release}
-
-mkdir ${BUILD_DIR} 2> /dev/null
-cd ${BUILD_DIR}
-
-echo "Configuring..."
-
-echo "cmake -C ${UMAP_DIR}/host-configs/${SYS_TYPE}/${COMPILER}.cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ${BUILD_OPTIONS} ../"
-cmake -C ${UMAP_DIR}/host-configs/${SYS_TYPE}/${COMPILER}.cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ${BUILD_OPTIONS} ../
-
-echo "Building..."
-make VERBOSE=1 -j
-
-echo "Testing..."
-# if [[ $HOSTNAME == *manta* ]]; then
-  # bsub -x -n 1 -G guests -Ip ctest -T Test
-# else
-  # srun -ppdebug -t 5 -N 1 ctest -T Test
-# fi
