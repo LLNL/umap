241c2d06ef7c06d28d9edb7cc10c0bdf0cb97a0f
Marty McFadden <mcfadden8@llnl.gov>
2018-09-06 16:06:52 -0700

    Updates for cmake

--- cmake/SetupUmapThirdParty.cmake
+++ cmake/SetupUmapThirdParty.cmake
@@ -1,25 +1,10 @@
 if ( ENABLE_CFITS )
-  find_library( T curl )
-  if ( T )
-    set (CFITS_CURL_LIB "curl")
-  endif()
-
-  find_library( T nsl )
-  if ( T )
-    set (CFITS_NSL_LIB "nsl")
-  endif()
-
-  find_library( T m )
-  if ( T )
-    set (CFITS_M_LIB "m")
-  endif()
-
-  find_library( CFITS_LIBRARY
+  find_library( CFITS_LIBRARIES
     libcfitsio.a
     PATHS ${CFITS_LIBRARY_PATH}
   )
 
-  if ( NOT CFITS_LIBRARY )
+if ( NOT CFITS_LIBRARIES )
     message( FATAL_ERROR "Could not find CFITS library, make sure CFITS_LIBRARY_PATH is set properly")
   endif()
 
@@ -30,6 +15,24 @@ if ( ENABLE_CFITS )
 
   if ( NOT CFITS_INCLUDE_DIR )
     message(FATAL_ERROR "Could not find CFITS include directory, make sure CFITS_INCLUDE_PATH is set properly")
+  endif()
+
+  unset (T)
+  find_library( T nsl )
+  if ( T )
+    set ( ${CFITS_LIBRARIES} ${T} )
+  endif()
+
+  unset (T)
+  find_library( T m )
+  if ( T )
+    set ( ${CFITS_LIBRARIES} ${T} )
+  endif()
+
+  unset (T)
+  find_library( T curl )
+  if ( T )
+    set ( ${CFITS_LIBRARIES} ${T} )
   endif()
 
   include_directories( ${CFITS_INCLUDE_DIR} )
