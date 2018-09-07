5127ba8b95241a814c39ca8990e5395ebf6cd6bb
Marty McFadden <mcfadden8@llnl.gov>
2018-09-07 10:31:47 -0700

    Second attempt at conditionally getting curl

--- cmake/SetupUmapThirdParty.cmake
+++ cmake/SetupUmapThirdParty.cmake
@@ -1,40 +1,15 @@
 if ( ENABLE_CFITS )
-  find_library( CFITS_LIBRARIES
-    libcfitsio.a
-    PATHS ${CFITS_LIBRARY_PATH}
-  )
+  FIND_PACKAGE(CURL)
 
-if ( NOT CFITS_LIBRARIES )
+  find_library( CFITS_LIBRARIES libcfitsio.a PATHS ${CFITS_LIBRARY_PATH} )
+  if ( NOT CFITS_LIBRARIES )
     message( FATAL_ERROR "Could not find CFITS library, make sure CFITS_LIBRARY_PATH is set properly")
   endif()
 
-  find_path( CFITS_INCLUDE_DIR
-    fitsio.h
-    PATHS ${CFITS_INCLUDE_PATH}
-  )
-
+  find_path( CFITS_INCLUDE_DIR fitsio.h PATHS ${CFITS_INCLUDE_PATH} )
   if ( NOT CFITS_INCLUDE_DIR )
     message(FATAL_ERROR "Could not find CFITS include directory, make sure CFITS_INCLUDE_PATH is set properly")
   endif()
 
-  unset (T)
-  find_library( T nsl )
-  if ( T )
-    set ( ${CFITS_LIBRARIES} ${T} )
-  endif()
-
-  unset (T)
-  find_library( T m )
-  if ( T )
-    set ( ${CFITS_LIBRARIES} ${T} )
-  endif()
-
-  unset (T)
-  find_library( T curl )
-  if ( T )
-    set ( ${CFITS_LIBRARIES} ${T} )
-  endif()
-
   include_directories( ${CFITS_INCLUDE_DIR} )
-
 endif()
