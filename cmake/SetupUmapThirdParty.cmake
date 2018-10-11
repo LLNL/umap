eeae44167bcf581756b018041d89c80a3e8b74c2
Marty McFadden <mcfadden8@llnl.gov>
2018-10-11 09:52:33 -0700

    Now bringing in curl for cfitsio library

--- cmake/SetupUmapThirdParty.cmake
+++ cmake/SetupUmapThirdParty.cmake
@@ -1,14 +1,15 @@
 if ( ENABLE_CFITS )
   FIND_PACKAGE(CURL)
+  if ( NOT CURL_FOUND )
+    message (FATAL_ERROR "Could not find CURL libraries which are needed by cfitsio")
+  endif()
 
   find_library( CFITS_LIBRARY libcfitsio.a PATHS ${CFITS_LIBRARY_PATH} )
   if ( NOT CFITS_LIBRARY )
     message( FATAL_ERROR "Could not find CFITS library, make sure CFITS_LIBRARY_PATH is set properly")
   endif()
 
-  find_library( BZ2_LIBRARY libbz2.so PATHS ${CFITS_LIBRARY_PATH} )
-  find_library( ZLIB_LIBRARY libz.so PATHS ${CFITS_LIBRARY_PATH} )
-  set(CFITS_LIBRARIES ${CFITS_LIBRARY} ${BZ2_LIBRARY} ${ZLIB_LIBRARY})
+  set(CFITS_LIBRARIES ${CFITS_LIBRARY} ${CURL_LIBRARIES} )
 
   find_path( CFITS_INCLUDE_DIR fitsio.h PATHS ${CFITS_INCLUDE_PATH} )
   if ( NOT CFITS_INCLUDE_DIR )
