579581c766b5696848bf1d500d57683d0220a055
Marty McFadden <mcfadden8@llnl.gov>
2019-02-20 13:43:04 -0800

    Updates to boilerplate license text

---
+++ scripts/check-license-info.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env zsh
+#############################################################################
+# Copyright 2017-2019 Lawrence Livermore National Security, LLC and other
+# UMAP Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: LGPL-2.1-only
+#############################################################################
+
+# This is used for the ~*tpl* line to ignore files in bundled tpls
+setopt extended_glob
+
+autoload colors
+
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+NOCOLOR="\033[0m"
+
+files_no_license=$(grep -L 'Lawrence Livermore National Security' \
+  cmake/**/*(^/) \
+  docs/**/*~*rst(^/)\
+  config/config.h.in\
+  examples/*(^/) \
+  host-configs/**/*(^/) \
+  scripts/**/*(^/) \
+  src/**/*~*tpl*(^/) \
+  tests/**/*~*csv~*xlsx~*sysinfo(^/) \
+  tests/CMakeLists.txt \
+  CMakeLists.txt)
+
+if [ $files_no_license ]; then
+  print "${RED} [!] Some files are missing license text: ${NOCOLOR}"
+  echo "${files_no_license}"
+  exit 255
+else
+  print "${GREEN} [Ok] All files have required license info."
+  exit 0
+fi
--- scripts/make_release_tarball.sh
+++ scripts/make_release_tarball.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #############################################################################
 # Copyright 2017-2019 Lawrence Livermore National Security, LLC and other
 # UMAP Project Developers. See the top-level COPYRIGHT file for details.
@@ -5,3 +6,9 @@
 # SPDX-License-Identifier: LGPL-2.1-only
 #############################################################################
 
+TAR_CMD=tar
+VERSION=0.0.4
+
+git archive --prefix=umap-${VERSION}/ -o umap-${VERSION}.tar HEAD 2> /dev/null
+
+gzip umap-${VERSION}.tar
