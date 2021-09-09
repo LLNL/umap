c60e6edccb8eda36cb11d7001b8c75feada13e29
asarkar-parsys <sarkar6@llnl.gov>
2021-09-08 20:48:57 -0700

    umap-client.h added

---
+++ src/umap/umap-client.h
@@ -0,0 +1,29 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define UMAP_SERVER_PATH "/tmp/umap-server"
+void init_umap_client(std::string sock_path);
+void close_umap_client();
+
+void* client_umap(
+    const char *filename
+  , int prot
+  , int flags
+  , void *addr
+);
+
+int client_uunmap(
+    const char *filename
+);
+long umapcfg_get_system_page_size( void );
+uint64_t umapcfg_get_max_pages_in_buffer( void );
+uint64_t umapcfg_get_umap_page_size( void );
+uint64_t umapcfg_get_num_fillers( void );
+uint64_t umapcfg_get_num_evictors( void );
+int umapcfg_get_evict_low_water_threshold( void );
+int umapcfg_get_evict_high_water_threshold( void );
+uint64_t umapcfg_get_max_fault_events( void );
+
+#ifdef __cplusplus
+}
+#endif
