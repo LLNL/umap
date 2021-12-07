ad3db023827d01612f0f0b28973fcec90f4ad1b0
IBPeng <56741918+IBPeng@users.noreply.github.com>
2021-12-07 11:10:53 -0800

    add timestamp when timestamp logging is activated (#118)
    * add timestamp when timestamp logging is activated

--- src/umap/util/Logger.cpp
+++ src/umap/util/Logger.cpp
@@ -13,6 +13,8 @@
 #include <sys/types.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <sstream>
+#include <iomanip>
 
 namespace Umap {
 
@@ -57,7 +59,11 @@ void Logger::logMessage( message::Level level,
 
   std::lock_guard<std::mutex> guard(g_logging_mutex);
   if (m_log_timestamp) {
+    std::ostringstream ss;
+    ss << std::fixed << std::setprecision(3) << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() / 1000.0;
+ 
     std::cout
+      << ss.str() << ":"
       << getpid() << ":"
       << syscall(__NR_gettid) << " "
       << "[" << MessageLevelName[ level ] << "]"
