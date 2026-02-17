ec641d46c781e9290e7ec7e3c68353e74c6e753c
Caleb Norton <n0603919@outlook.com>
2026-02-17 13:18:57 -0600

    chore: add nescessary headers

--- examples/bfs/bfs_kernel.hpp
+++ examples/bfs/bfs_kernel.hpp
@@ -15,6 +15,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 #ifndef BFS_BFS_KERNEL_HPP
 #define BFS_BFS_KERNEL_HPP
 
+#include <cstdint>
 #include <iostream>
 #include <limits>
 
